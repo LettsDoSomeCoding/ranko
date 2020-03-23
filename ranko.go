7d913edab3f645029dfd1fc851e50dc97f4f91cc
Rob <robjohn135@gmail.com>
2020-03-23 23:32:54 +0000

    remove missing dependencies from ranko.go

--- ranko.go
+++ ranko.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/LettsDoSomeCoding/ranko/api"
 )
 
 func main() {
 	fmt.Println("Hello I'm Ranko. I don't have a brain yet, but just you wait.")
-	api.StartServer()
-}
-
-func log(x int, y int) {
-	fmt.Println("hello")
 }
