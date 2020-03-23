ab404b4bb030cdf263af9fef4f20d5ad7e253423
Rob <robjohn135@gmail.com>
2020-03-23 23:27:07 +0000

    moving everything to the top layer and tweaking some stuff

--- logging/logging.go
+++ logging/logging.go
@@ -8,7 +8,7 @@ import (
 // todo - where should logs go to?
 var logger = log.New(os.Stderr, "API Server: ", log.Ldate|log.Ltime|log.Lmicroseconds|log.LUTC|log.Llongfile|log.Lmsgprefix)
 
-// GetLogger returns a singleton logger to be shared by all modules
+// GetLogger returns a singleton logger to be shared by all packages
 func GetLogger() *log.Logger {
 	return logger
 }
--- ranko.go
+++ ranko.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/LettsDoSomeCoding/ranko/api"
 )
 
 func main() {
 	fmt.Println("Hello I'm Ranko. I don't have a brain yet, but just you wait.")
+	api.StartServer()
+}
+
+func log(x int, y int) {
+	fmt.Println("hello")
 }
